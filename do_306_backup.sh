e34d00ea7f2e61809270d6a3cc4649aea1542afc
Linar <linar.nadyrov@itbegin.ru>
2019-09-09 23:08:09 +0300

    add do_306_backup.sh

--- do_306_backup.sh
+++ do_306_backup.sh
@@ -3,6 +3,6 @@
 DUMP_STORAGE=p1-nfs
 
 /usr/sbin/pct stop --skiplock 1 306
-vzdump 306 -compress gzip -storage=$DUMP_STORAGE --maxfiles 3 -mode stop --mailto admin@vip-connect.ru --mailnotification always
+vzdump 306 -compress gzip -storage=$DUMP_STORAGE --maxfiles 3 -mode stop --mailto admin@admin.ru --mailnotification always
 /usr/sbin/pct start 306
 
