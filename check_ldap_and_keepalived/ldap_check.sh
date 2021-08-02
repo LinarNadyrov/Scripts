73c512d8d0260e804957c2791ac750338ff04c82
LinarNadyrov <linar.nadyrov@itbegin.ru>
2021-08-02 14:59:05 +0300

    change PATH for scripts

--- check_ldap_and_keepalived/ldap_check.sh
+++ check_ldap_and_keepalived/ldap_check.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-(sleep 5 &&  python3 /opt/keepalived_start.py) &
-(sleep 10 && python3 /opt/keepalived_start.py) &
-(sleep 15 && python3 /opt/keepalived_start.py) &
-(sleep 20 && python3 /opt/keepalived_start.py) &
-(sleep 25 && python3 /opt/keepalived_start.py) &
-(sleep 30 && python3 /opt/keepalived_start.py) &
-(sleep 35 && python3 /opt/keepalived_start.py) &
-(sleep 40 && python3 /opt/keepalived_start.py) &
-(sleep 45 && python3 /opt/keepalived_start.py) &
-(sleep 50 && python3 /opt/keepalived_start.py) &
-(sleep 55 && python3 /opt/keepalived_start.py) &
-(sleep 60 && python3 /opt/keepalived_start.py) &
+LOCAL_PATH=/opt
+SCRIPT_FILE=keepalived_start.py
+
+(sleep 5 &&  python3 $LOCAL_PATH/$SCRIPT_FILE) &
+(sleep 10 && python3 $LOCAL_PATH/$SCRIPT_FILE) &
+(sleep 15 && python3 $LOCAL_PATH/$SCRIPT_FILE) &
+(sleep 20 && python3 $LOCAL_PATH/$SCRIPT_FILE) &
+(sleep 25 && python3 $LOCAL_PATH/$SCRIPT_FILE) &
+(sleep 30 && python3 $LOCAL_PATH/$SCRIPT_FILE) &
+(sleep 35 && python3 $LOCAL_PATH/$SCRIPT_FILE) &
+(sleep 40 && python3 $LOCAL_PATH/$SCRIPT_FILE) &
+(sleep 45 && python3 $LOCAL_PATH/$SCRIPT_FILE) &
+(sleep 50 && python3 $LOCAL_PATH/$SCRIPT_FILE) &
+(sleep 55 && python3 $LOCAL_PATH/$SCRIPT_FILE) &
+(sleep 60 && python3 $LOCAL_PATH/$SCRIPT_FILE) &
