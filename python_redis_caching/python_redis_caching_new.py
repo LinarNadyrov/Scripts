45234879bd37b099d9853c8830c6cff1f33f5aa5
LinarNadyrov <linar.nadyrov@itbegin.ru>
2024-01-08 22:56:13 +0300

    added new file for caching

---
+++ python_redis_caching/python_redis_caching_new.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+import os
+import redis
+from os.path import abspath
+from datetime import datetime
+import argparse
+
+# запуск - python3 python_work5.py -file /tmp/log_file_task5.log
+
+# При запуске принимаем в качестве аргумента путь к файлу 
+parser = argparse.ArgumentParser(description="command line options",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-file ', '--file_location', help="Source location")
+args = parser.parse_args()
+args_data = vars(args)
+
+# Новый список
+list_path = []
+# Словарь забираем в список
+for key, value in args_data.items():
+    list_path.append(value)
+text_file = list_path[0]
+
+# Создаем файл и записываем/перезаписываем в него данные
+text_data = [ '1string', '2string', '3string', '4string', '5string', '6string', '7string', '8string', '9string', '10string', '11string', '12string', '13string', '14string', '15string']
+#text_file = '/tmp/log_file_task5.log'
+
+with open(text_file, 'w', encoding='utf-8') as file:
+    for line in text_data:
+        file.write(line + '\n')
+
+# Определяем количество байтов в файле
+def all_bytes_from_file(FILE, NUMBER_BYTE):
+    # https://openwritings.net/pg/python/python-read-file-one-byte-time
+    with open(FILE, 'rb') as file:
+        byte = file.read(NUMBER_BYTE)         # Читаем файл c 1-го байта.
+        count = 0 
+        while byte:
+            byte = file.read(1)               # Читаем следующий байт.
+            count +=1                         # Суммируем количество байтов.
+        return count
+
+# Читаем файл с определенного байта и суммируем символы 
+def red_sum_n_bytes(FILE, NUMBER_BYTE):
+    with open(text_file, "rb") as file:
+        byte = file.read(NUMBER_BYTE)         # Читаем файл с N-го байта.
+        count = ''
+        while byte:
+            byte = file.read(1)               # Читаем следующий байт.
+            byte_to_str = str(byte.decode())
+            count = count + byte_to_str       # Суммируем количество символов.
+        return count
+
+from_this_byte = 100                          # Последние N байт файла
+
+# Подключение к redis и проверка существование запрашиваемого ключа
+# Скрипт читает файл только один раз, сколько бы не было вызовов с одинаковым аргументом.
+redis_client = redis.Redis(host = '127.0.0.1', port = 6379)
+
+# Если нет нужного ключа в Redis
+if redis_client.exists(text_file) == 0:
+    start_time = datetime.now()  # время начала выполнения
+    #print('No date')
+    
+    number_of_byte = all_bytes_from_file(text_file,1)
+    if number_of_byte == 100 or number_of_byte < 100:
+        list_byte_symfol = red_sum_n_bytes(text_file,1)
+        redis_client.set(text_file,list_byte_symfol)
+    else:
+        last_hundred_bytes = number_of_byte - from_this_byte
+        list_byte_symfol = red_sum_n_bytes(text_file,last_hundred_bytes)
+        redis_client.set(text_file,list_byte_symfol)
+
+    print(list_byte_symfol)
+
+    end_time = datetime.now()  # время окончания выполнения
+    execution_time = end_time - start_time  # вычисляем время выполнения
+    print(f'Program execution time: {execution_time} seconds')
+
+# Если нужный ключ в Redis существует
+elif redis_client.exists(text_file) == 1:
+    start_time = datetime.now()  # время начала выполнения
+    # Читаем нужный ключ и выходим данные в stdout
+    key = redis_client.get(text_file)
+    print(key.decode())
+    
+    end_time = datetime.now()  # время окончания выполнения
+    execution_time = end_time - start_time  # вычисляем время выполнения
+    print(f'Program execution time: {execution_time} seconds')
+
+# Ключ cmd-fcat должен содержать команду для запуска вашей программы 
+PATH_SCRIPTS = abspath(__file__)
+one_key = 'cmd-fcat'
+redis_client.set(one_key,PATH_SCRIPTS)
+#print('result for ', one_key, '= ', redis_client.get(one_key))
